f02c833a6d9704dc3e04110d80d69302b0db5eff
chenqin <490358423@qq.com>
2023-12-11 20:57:20 +0800

    Uniswap V2 FlashLoan

--- README.md
+++ README.md
@@ -16,9 +16,64 @@
 
 ## Uniswap
 
+安装依赖
+
+```
+forge install Uniswap/v2-core
+```
+
 ### v2
 
+v2中，闪电贷被写进了swap中，如果用户之前并没有向合约转入用于交易的代币，则相当于闪电贷。
 
+```solidity
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
+        require(amount0Out > 0 || amount1Out > 0, 'UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
+        (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
+        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'UniswapV2: INSUFFICIENT_LIQUIDITY');
+
+        uint balance0;
+        uint balance1;
+        { // scope for _token{0,1}, avoids stack too deep errors
+        address _token0 = token0; // gas saving
+        address _token1 = token1; // gas saving
+        require(to != _token0 && to != _token1, 'UniswapV2: INVALID_TO');
+        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
+        if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
+        // 如果使用闪电贷，则需要在自定义的uniswapV2Call方法中将借出的代币归还。
+        if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
+        balance0 = IERC20(_token0).balanceOf(address(this));
+        balance1 = IERC20(_token1).balanceOf(address(this));
+        }
+        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
+        uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
+        require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
+        { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
+        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
+        uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
+        // 由于在swap方法最后会检查余额（扣掉手续费后）符合k恒等式约束（参考白皮书公式），因此合约可以先将用户希望获得的代币转出，
+        // 新的K值必须大于等于之前的K值，理论上由于手续费，K值会不断变大
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K'); // 校验K值
+        }
+
+        _update(balance0, balance1, _reserve0, _reserve1); // 使用缓存余额更新价格预言机所需的累计价格，最后更新缓存余额为当前余额。
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+    }
+```
+
+看完这个方法，我们可以得出以下结论：
+
+- 最后的`require()`只检查了K值是否大于等于之前的K值，这就意味着，无论我们怎么借、还，只要K值满足条件，都可以调用成功。这就意味着我们可以有多种借款、还款方案：
+  - 借token0，还token0
+  - 借token0，还token1
+  - 借token0和token1，还token1
+  - 借token0和token1，还token0和token1
+  - 等等
+- 这就让我们有很大的灵活性：我们可以选择还任意的token，只要他的`价值`足够，比如：你借出了token0和token1，你可以选择还1000`token0`，或者还10`token1`，或者120`token0`+4`token1`。
+
+在我们写的使用教程中，写了四种不同的闪电贷，都可以通过。
+
+总结：在UniswapV2中，闪电贷的还款逻辑是价值取向
 
 ### v3
 
--- test/uniswap_v2.sol
+++ test/uniswap_v2.sol
@@ -1,24 +1,159 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '@uniswap/v2-core/contracts/interfaces/IERC20.sol';
+import "./interface.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract Uniswap_V2 is Test{
+    IUniswapV2Pair pair = IUniswapV2Pair(0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852); // pair(WETH-USDT)
+    IERC20 public constant WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IUSDT public constant USDT = IUSDT(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        vm.createSelectFork("mainnet", 18_647_450);
+        vm.label(address(pair), "Uniswap Pair");
+        vm.label(address(WETH), "WETH");
+        vm.label(address(USDT), "USDT");
+
+        // 我们需要借WETH，因此先准备一些WETH作为手续费
+        deal(address(WETH), address(this), 1 * 1e18);
+        deal(address(USDT), address(this), 1 * 1e18);
+
+        console.log("[the reserves]");
+        (, uint256 token0_reserve, uint256 token1_reserve) = pair.getReserves();
+        console.log("token0-WETH reserve", token0_reserve);
+        console.log("token1-USDT reserve", token1_reserve);
+        console.log();
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+// ============================================ 借token0，还token0 =========================================================
+
+    function test_flashloan1() public {
+        emit log_named_decimal_uint("[Before] WETH Balance", WETH.balanceOf(address(this)), WETH.decimals());
+
+        bytes memory data = abi.encode(address(WETH), 100_000_000);
+        // 我们需要借WETH，因此需要提前知道在这个池子中，token0和token1哪个是WETH，
+        // 第一个参数：本例中token0是WETH，因此第一个参数是要借出的WETH数量，我们借1_000_000；
+        // 第二个参数：要借的另外一个token
+        // 第三个参数：是借给谁；
+        // 第四个参数：是附带的data，可要可不要，如果写了，可以方便在回调函数中做相关处理，
+        // 我们编码了附带的data，为了在回调函数中解析出借了哪个代币，借了多少
+        pair.swap(100_000_000, 0, address(this), data);
+
+        emit log_named_decimal_uint("[After] WETH Balance", WETH.balanceOf(address(this)), WETH.decimals());
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function uniswapV2Call(address sender, uint256 amount0, uint256 amount1, bytes calldata data) external {
+        emit log_named_decimal_uint("[While] WETH Balance", WETH.balanceOf(address(this)), WETH.decimals());
+
+        // do anything you want
+
+        (address whatToBorrow, uint Amount) = abi.decode(data, (address, uint));
+        // 加上利息 0.3%
+        uint fee = ((Amount * 3) / 997) + 1;
+        uint paybackAmountAndFee = Amount + fee;
+        // console.log("You borrow:", whatToBorrow);
+
+        WETH.transfer(msg.sender, paybackAmountAndFee);
+
     }
+
+// ============================================== 借token0，还token1 =======================================================
+
+    // function test_flashloan2() public {
+    //     emit log_named_decimal_uint("[Before] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[Before] USDT Balance", USDT.balanceOf(address(this)), 18);
+    //     console.log();
+
+    //     bytes memory data = abi.encode(address(WETH), 100_000_000_000);
+    //     pair.swap(100_000_000_000, 0, address(this), data);
+
+    //     emit log_named_decimal_uint("[After] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[After] USDT Balance", USDT.balanceOf(address(this)), 18);
+    // }
+
+    // function uniswapV2Call(address, uint256, uint256, bytes calldata data) external {
+    //     emit log_named_decimal_uint("[While] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[While] USDT Balance", USDT.balanceOf(address(this)), 18);
+    //     console.log();
+
+    //     // do anything you want
+
+    //     (, uint256 Amount_usdt) = abi.decode(data, (address, uint256));
+    //     // 我们借了100_000_000_000的WETH，而只需要还210的USDT作为手续费，
+    //     // 说明了这个池子当中，USDT比较值钱（因为如果换 3/1000 的WETH，那么手续费远不止210）
+    //     USDT.transfer(msg.sender, 210); 
+    // }
+
+// ============================================== 借token0和token1，还token1 =======================================================
+
+    // function test_flashloan3() public {
+    //     emit log_named_decimal_uint("[Before] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[Before] USDT Balance", USDT.balanceOf(address(this)), 18);
+    //     console.log();
+
+    //     bytes memory data = abi.encode(address(WETH), address(USDT), 100_000_000, 200_000_000);
+    //     pair.swap(100_000_000, 200_000_000, address(this), data);
+
+    //     emit log_named_decimal_uint("[After] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[After] USDT Balance", USDT.balanceOf(address(this)), 18);
+    // }
+
+    // function uniswapV2Call(address, uint256, uint256, bytes calldata data) external {
+    //     emit log_named_decimal_uint("[While] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[While] USDT Balance", USDT.balanceOf(address(this)), 18);
+    //     console.log();
+
+    //     // do anything you want
+
+    //     (, , uint256 Amount_weth, uint256 Amount_usdt) = abi.decode(data, (address, address, uint256, uint256));
+    //     // 加上利息 0.3%
+    //     uint256 fee_weth = ((Amount_weth * 3) / 997) + 1;
+    //     uint256 fee_usdt = ((Amount_usdt * 3) / 997) + 1;
+    //     uint256 paybackAmountAndFee_weth = Amount_weth + fee_weth;
+    //     uint256 paybackAmountAndFee_usdt = Amount_usdt + fee_usdt;
+
+    //     // WETH.transfer(msg.sender, paybackAmountAndFee_weth);
+    //     // 我们并不需要两种代币都还手续费，只要我们交易前后的K值大于等于之前的K值，就可以通过，
+    //     // 我们这里设置为还WETH 0元，还USDT手续费加上借款金额。刚刚好足够手续费
+    //     // （也许是碰巧吧，USDT在本例中比较值钱，一般情况下要大于USDT手续费+借款金额）
+    //     WETH.transfer(msg.sender, 0);
+    //     USDT.transfer(msg.sender, paybackAmountAndFee_usdt);
+    // }
+
+// ============================================== 借token0和token1，还token0和token1 =======================================================
+    // function test_flashloan4() public {
+    //     emit log_named_decimal_uint("[Before] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[Before] USDT Balance", USDT.balanceOf(address(this)), 18);
+    //     console.log();
+
+    //     bytes memory data = abi.encode(address(WETH), address(USDT), 100_000_000, 200_000_000);
+    //     pair.swap(100_000_000, 200_000_000, address(this), data);
+
+    //     emit log_named_decimal_uint("[After] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[After] USDT Balance", USDT.balanceOf(address(this)), 18);
+    // }
+
+    // function uniswapV2Call(address, uint256, uint256, bytes calldata data) external {
+    //     emit log_named_decimal_uint("[While] WETH Balance", WETH.balanceOf(address(this)), 18);
+    //     emit log_named_decimal_uint("[While] USDT Balance", USDT.balanceOf(address(this)), 18);
+    //     console.log();
+
+    //     // do anything you want
+
+    //     (, , uint256 Amount_weth, uint256 Amount_usdt) = abi.decode(data, (address, address, uint256, uint256));
+    //     // 加上利息 0.3%
+    //     uint256 fee_weth = ((Amount_weth * 3) / 997) + 1;
+    //     uint256 fee_usdt = ((Amount_usdt * 3) / 997) + 1;
+    //     uint256 paybackAmountAndFee_weth = Amount_weth + fee_weth;
+    //     uint256 paybackAmountAndFee_usdt = Amount_usdt + fee_usdt;
+
+    //     WETH.transfer(msg.sender, 300_000_000);
+    //     USDT.transfer(msg.sender, paybackAmountAndFee_usdt - 1);
+    // }
 }
